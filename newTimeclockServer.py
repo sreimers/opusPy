a5ced8369c47e2448b904de188f84fee32057916
Ray Alfano <yajiu@Rays-MacBook-Pro.local>
2013-04-18 19:17:34 -0700

    Updates 4/18
    Experimentation with timeclock server

---
+++ newTimeclockServer.py
@@ -0,0 +1,33 @@
+import time
+import socket
+import os.path
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Prepare a server socket
+
+serverPort = 50001
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+
+while True:
+  #Establish connection
+  print 'Ready to serve...'
+  connectionSocket, addr = serverSocket.accept()
+
+  #Get Request
+  message = connectionSocket.recv(1024)
+  #Initialize time clock
+  currentTime = time.clock()
+  print currentTime
+  counter = 0
+
+  #Time clock
+  while counter < 100:
+      time.sleep(.01)
+      currentTime = time.clock()
+      print currentTime
+      counter = counter+1
+  break
+ 
+
